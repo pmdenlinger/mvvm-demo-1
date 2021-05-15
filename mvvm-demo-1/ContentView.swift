87a915f1b48ddf62c810c52a92972244ebf39568
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-15 10:33:57 -0700

    Initial Commit

---
+++ mvvm-demo-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvvm-demo-1
+//
+//  Created by user on 5/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mvvm-demo-1/mvvm_demo_1App.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_demo_1App.swift
+//  mvvm-demo-1
+//
+//  Created by user on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_demo_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
